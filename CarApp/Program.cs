ed3bbd29ffe1bfd3e601d818c8b5e4f1bb4a73b7
Andreas Hemmer <andreas.hemmer@hotmail.com>
2026-02-12 10:25:59 +0100

    Adds user query

--- CarApp/Program.cs
+++ CarApp/Program.cs
@@ -4,16 +4,27 @@
     {
         static void Main(string[] args)
         {
-           
+
+
+            /* Console.Write("Enter car brand: ");
+             string? carBrand = Console.ReadLine();
+             Console.Write("Enter car model: ");
+             string? modelType = Console.ReadLine();
+             Console.Write("Enter car year: ");
+             int year = Convert.ToInt32(Console.ReadLine());
+             Console.Write("Enter gear type: ");
+             char gearType = Console.ReadLine()[0];
+
+             Console.WriteLine();*/
+
+
+            Console.Write("Enter fuel type: ");
+            string? fuelType = Console.ReadLine();
+            Console.Write("Enter km/l: ");
+            double kmPerLiter = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter driving distance in km: ");
+            int drivingDistance = Convert.ToInt32(Console.ReadLine());
             
-            Console.Write("Enter car brand: ");
-            string? carBrand = Console.ReadLine();
-            Console.Write("Enter car model: ");
-            string? modelType = Console.ReadLine();
-            Console.Write("Enter car year: ");
-            int year = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter gear type: ");
-            char gearType = Console.ReadLine()[0];
 
             Console.WriteLine();
 
@@ -22,7 +33,11 @@
             //Console.WriteLine("Car Year: " + year);
             //Console.WriteLine("Gear Type: " + gearType);
 
-            Console.WriteLine("Your car is a " + carBrand + modelType + " from " + year + " with gear type: " +  gearType);
+            Console.WriteLine($"Fuel type: {fuelType}");
+            Console.WriteLine($"Km/l: {kmPerLiter}");
+            Console.WriteLine($"Driving distance: {drivingDistance}");
+            
+          
             
 
 
